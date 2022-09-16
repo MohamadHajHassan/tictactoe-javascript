58173d039dc09403e39dbbcde265c7a93cb7c5a5
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-16 21:22:05 +0300

    add check win fct

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,8 @@ window.onload = () => {
   const b8 = document.getElementById("8");
   const b9 = document.getElementById("9");
   let counter = 0;
+  let scoreYellow = 0;
+  let scoreRed = 0;
 
   // Functions
   const markBlock = item => {
@@ -18,14 +20,146 @@ window.onload = () => {
       if (item.childElementCount == 0) {
         if (counter % 2 == 0) {
           item.innerHTML = `<img src="images/yellow.png" alt="" />`;
+          console.log(item.innerHTML);
         } else {
           item.innerHTML = `<img src="images/red.png" alt="" />`;
         }
         counter++;
       }
     });
-  }
+  };
 
-  // 
+  const checkWin = () => {
+    if (
+      (b1.childElementCount == 1 &&
+        b2.childElementCount == 1 &&
+        b3.childElementCount == 1) ||
+      (b4.childElementCount == 1 &&
+        b5.childElementCount == 1 &&
+        b6.childElementCount == 1) ||
+      (b7.childElementCount == 1 &&
+        b8.childElementCount == 1 &&
+        b9.childElementCount == 1)
+    ) {
+      if (
+        (b1.innerHTML == b2.innerHTML &&
+          b1.innerHTML == b3.innerHTML &&
+          b1.innerHTML == `<img src="images/yellow.png" alt="">`) ||
+        (b4.innerHTML == b5.innerHTML &&
+          b4.innerHTML == b6.innerHTML &&
+          b4.innerHTML == `<img src="images/yellow.png" alt="">`) ||
+        (b7.innerHTML == b8.innerHTML &&
+          b7.innerHTML == b9.innerHTML &&
+          b7.innerHTML == `<img src="images/yellow.png" alt="">`)
+      ) {
+        // yellow win
+        scoreYellow++;
+        console.log(scoreRed, scoreYellow);
+        endOfRound();
+      } else if (
+        (b1.innerHTML == b2.innerHTML &&
+          b1.innerHTML == b3.innerHTML &&
+          b1.innerHTML == `<img src="images/red.png" alt="">`) ||
+        (b4.innerHTML == b5.innerHTML &&
+          b4.innerHTML == b6.innerHTML &&
+          b4.innerHTML == `<img src="images/red.png" alt="">`) ||
+        (b7.innerHTML == b8.innerHTML &&
+          b7.innerHTML == b9.innerHTML &&
+          b7.innerHTML == `<img src="images/red.png" alt="">`)
+      ) {
+        // red win
+        scoreRed++;
+        console.log(scoreRed, scoreYellow);
+        endOfRound();
+      }
+    }
+    if (
+      (b1.childElementCount == b4.childElementCount &&
+        b1.childElementCount == b7.childElementCount &&
+        b1.childElementCount == 1) ||
+      (b2.childElementCount == b5.childElementCount &&
+        b2.childElementCount == b8.childElementCount &&
+        b2.childElementCount == 1) ||
+      (b3.childElementCount == b6.childElementCount &&
+        b3.childElementCount == b9.childElementCount &&
+        b3.childElementCount == 1)
+    ) {
+      if (
+        (b1.innerHTML == b4.innerHTML &&
+          b1.innerHTML == b7.innerHTML &&
+          b1.innerHTML == `<img src="images/yellow.png" alt="">`) ||
+        (b2.innerHTML == b5.innerHTML &&
+          b2.innerHTML == b8.innerHTML &&
+          b2.innerHTML == `<img src="images/yellow.png" alt="">`) ||
+        (b3.innerHTML == b6.innerHTML &&
+          b3.innerHTML == b9.innerHTML &&
+          b3.innerHTML == `<img src="images/yellow.png" alt="">`)
+      ) {
+        // yellow win
+        scoreYellow++;
+        console.log(scoreRed, scoreYellow);
+        endOfRound();
+      } else if (
+        (b1.innerHTML == b4.innerHTML &&
+          b1.innerHTML == b7.innerHTML &&
+          b1.innerHTML == `<img src="images/red.png" alt="">`) ||
+        (b2.innerHTML == b5.innerHTML &&
+          b2.innerHTML == b8.innerHTML &&
+          b2.innerHTML == `<img src="images/red.png" alt="">`) ||
+        (b3.innerHTML == b6.innerHTML &&
+          b3.innerHTML == b9.innerHTML &&
+          b3.innerHTML == `<img src="images/red.png" alt="">`)
+      ) {
+        // red win
+        scoreRed++;
+        console.log(scoreRed, scoreYellow);
+        endOfRound();
+      }
+    }
+    if (
+      (b1.childElementCount == b5.childElementCount &&
+        b1.childElementCount == b9.childElementCount &&
+        b1.childElementCount == 1) ||
+      (b7.childElementCount == b5.childElementCount &&
+        b7.childElementCount == b3.childElementCount &&
+        b7.childElementCount == 1)
+    ) {
+      if (
+        (b1.innerHTML == b5.innerHTML &&
+          b1.innerHTML == b9.innerHTML &&
+          b1.innerHTML == `<img src="images/yellow.png" alt="">`) ||
+        (b7.innerHTML == b5.innerHTML &&
+          b7.innerHTML == b3.innerHTML &&
+          b7.innerHTML == `<img src="images/yellow.png" alt="">`)
+      ) {
+        // yellow win
+        scoreYellow++;
+        console.log(scoreRed, scoreYellow);
+        endOfRound();
+      } else if (
+        (b1.innerHTML == b5.innerHTML &&
+          b1.innerHTML == b9.innerHTML &&
+          b1.innerHTML == `<img src="images/red.png" alt="">`) ||
+        (b7.innerHTML == b5.innerHTML &&
+          b7.innerHTML == b3.innerHTML &&
+          b7.innerHTML == `<img src="images/red.png" alt="">`)
+      ) {
+        // red win
+        scoreRed++;
+        console.log(scoreRed, scoreYellow);
+        endOfRound();
+      }
+    }
+  };
+
+  const ListenerCheckWin = item => {
+    item.addEventListener("click", checkWin);
+  };
+
+  const endOfRound = () => {
+    blocks.forEach(item => (item.innerHTML = ""));
+  };
+  //
   blocks.forEach(markBlock);
+  blocks.forEach(ListenerCheckWin);
 };
